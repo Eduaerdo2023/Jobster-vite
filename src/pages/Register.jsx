ca8bb6e2bc4c20d802b0346d544833a12aba66c5
Eduardo2023 <ebesquin@gmail.com>
2023-12-20 15:59:01 -0600

    Capitulo541

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from 'react'
 import { FormRow, Logo } from '../components'
 import Wrapper from '../assets/wrappers/RegisterPage'
+import { toast } from 'react-toastify'
 
 const initialState = {
   name: '',
@@ -24,7 +25,7 @@ const Register = () => {
     e.preventDefault()
     const { name, email, password, isMember } = values
     if (!email || !password || (!isMember && !name)) {
-      console.log('fillout');
+      toast.error('please fill out all the fields')
       return
     }
   }
