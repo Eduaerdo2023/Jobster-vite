de34c1bd4c933510520abfa14a0eb858b610f75e
Eduardo2023 <ebesquin@gmail.com>
2023-12-27 09:43:41 -0600

    Capitulo588

--- src/pages/dashboard/AllJobs.jsx
+++ src/pages/dashboard/AllJobs.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import { JobsContainer, SearchContainer } from "../../components"
+
 
 
 const AllJobs = () => {
   return (
-    <div>
-      alljobs
-    </div>
+    <>
+      <SearchContainer />
+      <JobsContainer />
+    </>
+
   )
 }
 
